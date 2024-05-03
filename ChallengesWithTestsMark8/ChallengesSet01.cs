9294c7e93110b4456e7c4c3bc488a7d33072132c
JP Mohr <James.Mohr2.0@hotmail.com>
2024-05-03 10:57:18 -0400

    challenge set 01 done

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,44 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            //  throw new NotImplementedException();
+            var answer = minuend - subtrahend;
+            return answer;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var sum = number1 + number2;
+            return sum;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var smol = ((number1 < number2) ? number1 : number2);
+            return smol;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var multi = factor1 * factor2;
+            return multi;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return ($"Good morning {nameOfPerson}, how are you today?");
+
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var hey = Console.ReadLine();
+            return hey;
         }
     }
 }
